755342f1a16dad72ab6cf6186dbd640f717b5f6f
Thai Anh Van Vu <cunhivu@gmail.com>
2022-06-02 16:41:10 -0700

    Integrated SQLite database and set up baseline for Firebase

--- app/src/main/java/com/isit322/plant_tracker/PlantInput.kt
+++ app/src/main/java/com/isit322/plant_tracker/PlantInput.kt
@@ -2,15 +2,20 @@ package com.isit322.plant_tracker
 
 import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.FileProvider
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.io.File
 
 //Camera functions
@@ -20,7 +25,16 @@ import java.io.File
 
 
 class PlantInput : AppCompatActivity() {
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+    private var filePath: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val plantDatabase = openOrCreateDatabase("PlantDatabaseTest", MODE_PRIVATE, null)
+
+        plantDatabase.execSQL("CREATE TABLE IF NOT EXISTS PlantTable(PlantID integer primary key autoincrement, PlantName VARCHAR, Location VARCHAR, Description VARCHAR, RelativePath VARCHAR);")
+
         lateinit var photoFile: File
         val REQUEST_CODE = 42
         val FILE_NAME = "photo"
@@ -30,6 +44,33 @@ class PlantInput : AppCompatActivity() {
 
         //Sets up the picture button, using an Intent to access the camera to actually take the photo
         val btnTakePicture = findViewById<Button>(R.id.label_PlantPicture)
+        val btnSubmit = findViewById<Button>(R.id.AddPlantBtn)
+
+        btnSubmit.setOnClickListener {
+            val plantName = findViewById<TextView>(R.id.PlantName).text
+            val plantLocation = findViewById<TextView>(R.id.Location).text
+            val plantDescription = findViewById<TextView>(R.id.PlantDescription).text
+
+            plantDatabase.execSQL("INSERT INTO PlantTable VALUES (NULL, '$plantName', '$plantLocation', '$plantDescription', NULL);")
+
+            val newIDRaw: Cursor = plantDatabase.rawQuery("SELECT MAX(PlantID) FROM PlantTable", null)
+            newIDRaw.moveToFirst()
+            val finalID = newIDRaw.getString(0).toInt()
+            val relPath = "image_$finalID.png"
+            plantDatabase.execSQL("UPDATE PlantTable SET RelativePath = \"" + relPath +
+                    "\" WHERE PlantID = " + finalID)
+            Toast.makeText(this, "Plant added to database", Toast.LENGTH_SHORT).show()
+
+            val newPlantName: Cursor = plantDatabase.rawQuery("SELECT PlantName FROM PlantTable WHERE PlantID = $finalID", null)
+            newPlantName.moveToFirst()
+            val plantNameDisplay = newPlantName.getString(0)
+
+
+            findViewById<Button>(R.id.AddPlantBtn).text = "$plantNameDisplay"
+            
+        }
+
+
         btnTakePicture.setOnClickListener {
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             photoFile = getPhotoFile(FILE_NAME)
@@ -44,6 +85,9 @@ class PlantInput : AppCompatActivity() {
             }
         }
     }
+
+
+
 
     //gets the directory for pictures and places a new one for the photo to be taken
     private fun getPhotoFile(fileName: String): File {
