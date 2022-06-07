a0cc03650088b69e754b090916b6d24d67d8595a
Your Name <samtshiek@yahoo.fr>
2022-06-07 15:46:55 -0700

    Hostname added instead of ip address

--- app/src/main/java/com/isit322/plant_tracker/utils/Config.kt
+++ app/src/main/java/com/isit322/plant_tracker/utils/Config.kt
@@ -2,7 +2,7 @@ package com.isit322.artworklist.utils
 
 class Config {
     companion object {
-        const val BASE_URL = "http://34.168.250.89/"
+        const val BASE_URL = "http://veggiecrusader.ddns.net/"
         const val SEARCH_ENDPOINT = "GetAllData"
         const val GEO_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json?"
         const val GEO_API_KEY = "AIzaSyDlcVUDD3WhvXXA2XvrTflCjMn0VO3Bam8"
