8693960a23920007b2eccbb3df764b39c735b54f
Sean <seankel@comcast.net>
2022-06-16 17:16:35 -0700

    Added API test

--- app/src/main/java/com/isit322/plant_tracker_test/InstrumentedTest.kt
+++ app/src/main/java/com/isit322/plant_tracker_test/InstrumentedTest.kt
@@ -1,10 +1,17 @@
 package com.isit322.plant_tracker_test
 
+import android.util.Log
+import com.isit322.artworklist.api.ApiClient
+import com.isit322.artworklist.data.Plant
+import com.isit322.artworklist.utils.Config
 import com.isit322.plant_tracker.Validation
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import retrofit2.Call
+import retrofit2.Response
+
 
 @RunWith(JUnit4::class)
 class InstrumentTest {
@@ -25,5 +32,30 @@ class InstrumentTest {
         val desc = "two"
         val result = Validation.returnPlantObjectDescription(desc)
         assertEquals(result, desc)
+    }
+
+    @Test
+    fun plant_data_test() {
+        val TAG = "plant_request"
+        val get_plant_url = Config.BASE_URL + Config.GET_ENDPOINT
+        val call = ApiClient.getUserApiService().getPlants(get_plant_url)
+        call.enqueue(object:retrofit2.Callback<Plant> {
+            override fun onResponse(call: Call<Plant>, response: Response<Plant>) {
+                if (response.isSuccessful) {
+                    Log.d(TAG, "Response is successful. Response: ${response.body()?.get(0)?.plantName}")
+                    val  result =  response.body()?.get(0)?.plantName
+                    val expected = "Sun flower"
+                    assertEquals( expected, result)
+                }
+                else {
+                    Log.d(TAG, "Response is NOT successful.")
+                }
+
+            }
+
+            override fun onFailure(call: Call<Plant>, t: Throwable) {
+                Log.d(TAG, "Request failed. Message: {${t.message}")
+            }
+        })
     }
 }
